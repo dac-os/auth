c0faa6c9ad9480beebc2ed0afff6121b1dbac5fa
Lamasu <rafaerthal@gmail.com>
2014-08-22 01:03:49 -0300

    docs for profile

--- README.md
+++ README.md
@@ -1,4 +1,204 @@
-auth
-====
+# dacos-auth v0.0.1
 
-[ ![Codeship Status for dac-os/auth](https://www.codeship.io/projects/33182bd0-0bd9-0132-22a0-72230f3fc4ea/status)](https://www.codeship.io/projects/32069)
+autenticação do sistema da diretoria academica DAC
+
+- [profile](#profile)
+	- [Creates a new profile.](#creates-a-new-profile.)
+	- [Get profile information.](#get-profile-information.)
+	- [List all system profiles.](#list-all-system-profiles.)
+	- [Removes profile.](#removes-profile.)
+	- [Updates profile information.](#updates-profile-information.)
+	
+
+
+# profile
+
+## Creates a new profile.
+
+When creating a new profile the user must send the profile name and the profile permissions. The profile name is used
+for naming and must be unique in the system. If a existing name is sent to this method, a 409 error will be raised.
+And if no name is sent, a 400 error will be raised. The profile permissions consists of an array of strings
+representing the actions the user can do on the system.
+
+	POST /profiles
+
+### Parameters
+
+| Name    | Type      | Description                          |
+|---------|-----------|--------------------------------------|
+| name			| String			|  Profile name.							|
+| permissions			| [String]			|  List of profile permissions.							|
+
+### Success Response
+
+HTTP/1.1 201 Created
+
+```
+{}
+
+```
+### Error Response
+
+HTTP/1.1 400 Bad Request
+
+```
+{
+ "name": "required"
+}
+
+```
+HTTP/1.1 403 Forbidden
+
+```
+{}
+
+```
+HTTP/1.1 409 Conflict
+
+```
+{}
+
+```
+## Get profile information.
+
+This method returns a single profile details, the profile slug must be passed in the uri to identify the requested
+profile. If no profile with the requested slug was found, a 404 error will be raised.
+
+	GET /profiles/:profile
+
+
+### Success Response
+
+HTTP/1.1 200 OK
+
+```
+{
+ "slug": "teacher",
+ "name": "teacher",
+ "permissions": [
+   "changeGrades"
+ ],
+ "createdAt": "2014-07-01T12:22:25.058Z",
+ "updatedAt": "2014-07-01T12:22:25.058Z"
+}
+
+```
+### Error Response
+
+HTTP/1.1 404 Not Found
+
+```
+{}
+
+```
+## List all system profiles.
+
+This method returns an array with all profiles in the database. The data is returned in pages of length 20. If no
+page is passed, the system will assume the requested page is page 0, otherwise the desired page must be sent.
+
+	GET /profiles
+
+### Parameters
+
+| Name    | Type      | Description                          |
+|---------|-----------|--------------------------------------|
+| page			| [Number=0]			|  Requested page.							|
+
+### Success Response
+
+HTTP/1.1 200 OK
+
+```
+[{
+ "slug": "teacher",
+ "name": "teacher",
+ "permissions": [
+   "changeGrades"
+ ],
+ "createdAt": "2014-07-01T12:22:25.058Z",
+ "updatedAt": "2014-07-01T12:22:25.058Z"
+}]
+
+```
+## Removes profile.
+
+This method removes a profile from the system. If no profile with the requested slug was found, a 404 error will be
+raised.
+
+	POST /profiles
+
+
+### Success Response
+
+HTTP/1.1 204 No Content
+
+```
+{}
+
+```
+### Error Response
+
+HTTP/1.1 404 Not Found
+
+```
+{}
+
+```
+HTTP/1.1 403 Forbidden
+
+```
+{}
+
+```
+## Updates profile information.
+
+When updating a profile the user must send the profile name and the profile permissions. If a existing name which is
+not the original profile name is sent to this method, a 409 error will be raised. And if no name is sent, a 400 error
+will be raised.  If no profile with the requested slug was found, a 404 error will be raised.
+
+	POST /profiles
+
+### Parameters
+
+| Name    | Type      | Description                          |
+|---------|-----------|--------------------------------------|
+| name			| String			|  Profile name.							|
+| permissions			| [String]			|  List of profile permissions.							|
+
+### Success Response
+
+HTTP/1.1 200 Ok
+
+```
+{}
+
+```
+### Error Response
+
+HTTP/1.1 404 Not Found
+
+```
+{}
+
+```
+HTTP/1.1 400 Bad Request
+
+```
+{
+ "name": "required"
+}
+
+```
+HTTP/1.1 403 Forbidden
+
+```
+{}
+
+```
+HTTP/1.1 409 Conflict
+
+```
+{}
+
+```
+
--- controllers/profile.js
+++ controllers/profile.js
@@ -7,6 +7,39 @@ slug = require('slug');
 auth = require('../auth');
 Profile = require('../models/profile');
 
+/**
+ * @api {post} /profiles Creates a new profile.
+ * @apiName createProfile
+ * @apiVersion 1.0.0
+ * @apiGroup profile
+ * @apiPermission changeProfile
+ * @apiDescription
+ * When creating a new profile the user must send the profile name and the profile permissions. The profile name is used
+ * for naming and must be unique in the system. If a existing name is sent to this method, a 409 error will be raised.
+ * And if no name is sent, a 400 error will be raised. The profile permissions consists of an array of strings
+ * representing the actions the user can do on the system.
+ *
+ * @apiParam {String} name Profile name.
+ * @apiParam {[String]} permissions List of profile permissions.
+ *
+ * @apiErrorExample
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "name": "required"
+ * }
+ *
+ * @apiErrorExample
+ * HTTP/1.1 403 Forbidden
+ * {}
+ *
+ * @apiErrorExample
+ * HTTP/1.1 409 Conflict
+ * {}
+ *
+ * @apiSuccessExample
+ * HTTP/1.1 201 Created
+ * {}
+ */
 router
 .route('/profiles')
 .post(auth.can('changeProfile'))
@@ -28,6 +61,36 @@ router
   });
 });
 
+/**
+ * @api {get} /profiles List all system profiles.
+ * @apiName listProfile
+ * @apiVersion 1.0.0
+ * @apiGroup profile
+ * @apiPermission none
+ * @apiDescription
+ * This method returns an array with all profiles in the database. The data is returned in pages of length 20. If no
+ * page is passed, the system will assume the requested page is page 0, otherwise the desired page must be sent.
+ *
+ * @apiParam {[Number=0]} page Requested page.
+ *
+ * @apiSuccess (profile) {String} slug Profile identifier.
+ * @apiSuccess (profile) {String} name Profile name.
+ * @apiSuccess (profile) {[String]} permissions List of profile permissions.
+ * @apiSuccess (profile) {date} createdAt Profile creation date.
+ * @apiSuccess (profile) {date} updatedAt Profile last update date.
+ *
+ * @apiSuccessExample
+ * HTTP/1.1 200 OK
+ * [{
+ *   "slug": "teacher",
+ *   "name": "teacher",
+ *   "permissions": [
+ *     "changeGrades"
+ *   ],
+ *   "createdAt": "2014-07-01T12:22:25.058Z",
+ *   "updatedAt": "2014-07-01T12:22:25.058Z"
+ * }]
+ */
 router
 .route('/profiles')
 .get(function listProfile(request, response, next) {
@@ -48,6 +111,38 @@ router
   });
 });
 
+/**
+ * @api {get} /profiles/:profile Get profile information.
+ * @apiName getProfile
+ * @apiVersion 1.0.0
+ * @apiGroup profile
+ * @apiPermission none
+ * @apiDescription
+ * This method returns a single profile details, the profile slug must be passed in the uri to identify the requested
+ * profile. If no profile with the requested slug was found, a 404 error will be raised.
+ *
+ * @apiSuccess {String} slug Profile identifier.
+ * @apiSuccess {String} name Profile name.
+ * @apiSuccess {[String]} permissions List of profile permissions.
+ * @apiSuccess {date} createdAt Profile creation date.
+ * @apiSuccess {date} updatedAt Profile last update date.
+ *
+ * @apiErrorExample
+ * HTTP/1.1 404 Not Found
+ * {}
+ *
+ * @apiSuccessExample
+ * HTTP/1.1 200 OK
+ * {
+ *   "slug": "teacher",
+ *   "name": "teacher",
+ *   "permissions": [
+ *     "changeGrades"
+ *   ],
+ *   "createdAt": "2014-07-01T12:22:25.058Z",
+ *   "updatedAt": "2014-07-01T12:22:25.058Z"
+ * }
+ */
 router
 .route('/profiles/:profile')
 .get(function getProfile(request, response) {
@@ -58,6 +153,42 @@ router
   return response.status(200).send(profile);
 });
 
+/**
+ * @api {post} /profiles Updates profile information.
+ * @apiName updateProfile
+ * @apiVersion 1.0.0
+ * @apiGroup profile
+ * @apiPermission changeProfile
+ * @apiDescription
+ * When updating a profile the user must send the profile name and the profile permissions. If a existing name which is
+ * not the original profile name is sent to this method, a 409 error will be raised. And if no name is sent, a 400 error
+ * will be raised.  If no profile with the requested slug was found, a 404 error will be raised.
+ *
+ * @apiParam {String} name Profile name.
+ * @apiParam {[String]} permissions List of profile permissions.
+ *
+ * @apiErrorExample
+ * HTTP/1.1 404 Not Found
+ * {}
+ *
+ * @apiErrorExample
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "name": "required"
+ * }
+ *
+ * @apiErrorExample
+ * HTTP/1.1 403 Forbidden
+ * {}
+ *
+ * @apiErrorExample
+ * HTTP/1.1 409 Conflict
+ * {}
+ *
+ * @apiSuccessExample
+ * HTTP/1.1 200 Ok
+ * {}
+ */
 router
 .route('/profiles/:profile')
 .put(auth.can('changeProfile'))
@@ -78,6 +209,28 @@ router
   });
 });
 
+/**
+ * @api {post} /profiles Removes profile.
+ * @apiName removeProfile
+ * @apiVersion 1.0.0
+ * @apiGroup profile
+ * @apiPermission changeProfile
+ * @apiDescription
+ * This method removes a profile from the system. If no profile with the requested slug was found, a 404 error will be
+ * raised.
+ *
+ * @apiErrorExample
+ * HTTP/1.1 404 Not Found
+ * {}
+ *
+ * @apiErrorExample
+ * HTTP/1.1 403 Forbidden
+ * {}
+ *
+ * @apiSuccessExample
+ * HTTP/1.1 204 No Content
+ * {}
+ */
 router
 .route('/profiles/:profile')
 .delete(auth.can('changeProfile'))
